30b73ec82bea93e6101b3d37dcec787145f79d82
Damon Kalaboukis <damonkala@yahoo.com>
2015-11-17 00:34:53 -0800

    I FORGOT A FILE

---
+++ public/js/pageConfig.js
@@ -0,0 +1,42 @@
+'use strict';
+
+$(document).ready(init);
+
+function init() {
+
+
+  $('#logout').click(logout);
+  $('#changeInfo').click(changeInfo);
+  $('#saveChanges').click(saveChanges);
+  
+}
+
+function changeInfo(){
+  $('#saveChanges').attr('hidden', false);
+  $('#bio').attr('contentEditable', true);
+}
+
+function saveChanges() {
+  var username = $('#title').text();
+  //   $('#saveChanges').attr('hidden', true);
+    $('#bio').attr('contentEditable', false);
+    var bio = $('#bio').text()
+    $.ajax({
+  url: '/users/update',
+  type: 'PUT',
+  data: {
+    name: username,
+    bio: bio
+  },
+  success: function(result) {
+      console.log('edited')
+  }
+})
+
+function logout() {
+  $.post('/users/logout')
+  .done(function(){
+    window.location.replace('/');
+  });
+}
+}
